3781485e59f43d27db0d4e9ba459075f8c026ed2
Arash Mozhdehi <armozhdehi@gmail.com>
2022-04-27 19:09:09 -0600

    Smoothing the data and noise reduction using extended kalman diltering through the new method

--- application.py
+++ application.py
@@ -26,6 +26,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+kf = KalmanFilter();
 
 config = {
   "apiKey": "AIzaSyCmj02DX8-_GpQiMc35-VUis84LLk4FGcU",
